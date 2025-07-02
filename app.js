9990f12cee7189364adac977bb9c078f018e63cc
Aman <amanfarhan2008@gmail.com>
2025-07-02 16:48:09 +0500

    modify javascript

---
+++ app.js
@@ -0,0 +1,60 @@
+// function sum (num1 , num2){
+//     console.log(num1 + num2);
+// }
+
+// sum(2,2)
+
+// function sum (num1 , num2){
+//     console.log(num1 + num2);
+// }
+
+// let num1 = prompt("enter your Name");
+// let  = prompt("enter your last Name");
+
+// sum(num1);
+// sum(num2);
+// console.log("welcome" + num1 + num2 + "!")
+
+// function username (userInput,userInput2){
+//     let userInput = prompt("enter you first name");
+//     let userInput2 = prompt("enter you first name");
+//     console.log("welcome" + userInput + userInput2 + "!")
+// }
+// username()
+
+// function greet (userName) {
+//     return "welcome " + userName + "!";
+// }
+
+// function userName (name){
+//     let message = greet(name)
+//     console.log(message)
+// }
+// userName("aman")
+
+
+// function greek (num1,num2) {
+//     return num1 + num2  ;
+// }
+
+// function add (name){
+//     let message = greek(name);
+//     console.log(message)
+// }
+// userName( +prompt())
+// userName( +prompt())
+
+
+// function greek (num1,num2) {
+//     return num1 + num2;
+// }
+
+// function add (userNum){
+//     let message = greek(userNum);
+//     console.log(message);
+// }
+// let userNum = prompt("enter a number");
+// let userNum1 = prompt("enter a second number");
+
+userNum(num1 , num2);
+
